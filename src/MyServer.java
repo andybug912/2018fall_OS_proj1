65aabe74c5a10ebca6555c13ae5e9a21c249a693
andizhang <andybug@hotmail.com>
2018-10-05 16:53:34 -0400

    add empty contruct & start & main

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class MyServer {
-    
+    public MyServer(int port) {
+
+    }
+
+    public void start() {
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input server port:");
+        String port = scanner.nextLine();
+        MyServer server = new MyServer(Integer.parseInt(port));
+        server.start();
+    }
 }
