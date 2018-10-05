b68a694ad9efc6908867ab3325f628412af0c7ec
andizhang <andybug@hotmail.com>
2018-10-05 17:38:24 -0400

    read server info from local file

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,15 +1,42 @@
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
 
 public class MyServer {
     private int myIndex;
     private int totalNumOfServers;
+    private int myPort;
 
     public MyServer(int port) {
-
+        
     }
 
     public void start() {
-        String serverInfo = "serverlist.txt";
+        String serverInfoFile = "server_list.txt";
+        File file = new File(serverInfoFile);
+        Scanner fileScanner;
+        try {
+            fileScanner = new Scanner(file);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        List<List<String[]>> otherServers = new ArrayList<>();
+        int i = 0;
+        while (fileScanner.hasNext() && i < this.totalNumOfServers) {
+            String[] serverInfo = fileScanner.nextLine().split(" ");
+            if (i == this.myIndex) {
+                this.myPort = Integer.parseInt(serverInfo[1]);
+            }
+            else {
+                List<String[]> tempList = new ArrayList<>();
+                tempList.add(serverInfo);
+                otherServers.add(tempList);
+            }
+            i++;
+        }
+        // TODO (Zhiben Zhu): add serverSocket
 
     }
 
