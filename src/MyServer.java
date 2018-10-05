d62e92c137e5f2f00d3a9c88f8d588504679c7e3
andizhang <andybug@hotmail.com>
2018-10-05 18:25:23 -0400

    add connect to other servers

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,3 +1,6 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.File;
@@ -8,10 +11,14 @@ public class MyServer {
     private int myIndex;
     private int totalNumOfServers;
     private int myPort;
+    private List<ObjectOutputStream> outputStreams;
+    private List<ObjectInputStream> inputStreams;
 
     public MyServer(int totalNumOfServers, int myIndex) {
         this.myIndex=myIndex;
         this.totalNumOfServers=totalNumOfServers;
+        outputStreams = new ArrayList<>();
+        inputStreams = new ArrayList<>();
     }
 
     public void start() {
@@ -25,7 +32,7 @@ public class MyServer {
             System.out.println(e.getMessage());
             return;
         }
-        List<List<String[]>> otherServers = new ArrayList<>();
+        List<String[]> otherServers = new ArrayList<>();
         int i = 0;
         while (fileScanner.hasNext() && i < this.totalNumOfServers) {
             String[] serverInfo = fileScanner.nextLine().split(" ");
@@ -33,13 +40,37 @@ public class MyServer {
                 this.myPort = Integer.parseInt(serverInfo[1]);
             }
             else {
-                List<String[]> tempList = new ArrayList<>();
-                tempList.add(serverInfo);
-                otherServers.add(tempList);
+                otherServers.add(serverInfo);
             }
             i++;
         }
+        fileScanner.close();
         // TODO (Zhiben Zhu): add serverSocket
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter to connect to other servers...");
+        scanner.nextLine();
+        connectOtherSevers(otherServers);
+
+
+    }
+
+    private void connectOtherSevers(List<String[]> otherServers) {
+        for (String[] server: otherServers) {
+            String serverName = server[0];
+            int port = Integer.parseInt(server[1]);
+            try {
+                Socket socket = new Socket(server[0], Integer.parseInt(server[1]));
+                ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+                this.outputStreams.add(output);
+                this.inputStreams.add(input);
+                System.out.println("Connected to " + serverName + " on port " + port);
+            }
+            catch (Exception e) {
+                System.err.println("Failed to connect to " + serverName + " on port " + port + "\nError: " + e.getMessage());
+                e.printStackTrace(System.err);
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException{
