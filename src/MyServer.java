8618d4f0e40262aae49dc42c606cf90a6cf4d8bf
Ben <zhuzhiben@fq-wifi-10-215-189-82.wireless.pitt.edu>
2018-10-05 17:46:59 -0400

    read config

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MyServer {
     private int myIndex;
     private int totalNumOfServers;
 
-    public MyServer(int port) {
-
+    public MyServer(int totalNumOfServers, int myIndex) {
+        this.myIndex=myIndex;
+        this.totalNumOfServers=totalNumOfServers;
     }
 
     public void start() {
@@ -13,11 +18,18 @@ public class MyServer {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input server port:");
-        String port = scanner.nextLine();
-        MyServer server = new MyServer(Integer.parseInt(port));
+        System.out.println("Input total numbers of servers:");
+        String tnfs = scanner.nextLine();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Input myIndex:");
+        String mi = scanner2.nextLine();
+
+        MyServer server = new MyServer(Integer.parseInt(tnfs),Integer.parseInt(mi));
         server.start();
     }
+
+
 }
